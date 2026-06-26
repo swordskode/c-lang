8bea030e87699e6699028c79387af8432d1fe0ec
David Swords <david@swords.computer>
2026-06-26 18:36:36 +0200

    Added 04_comparisons.c

---
+++ 04_comparisons.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 10;
+    int b = 20;
+
+    printf("a = %d\n", a);
+    printf("b = %d\n\n", b);
+
+    printf("a == b : %d\n", a == b);
+    printf("a != b : %d\n", a != b);
+    printf("a < b : %d\n", a < b);
+    printf("a <= b : %d\n", a <= b);
+    printf("a > b : %d\n", a > b);
+    printf("a >= b : %d\n", a >= b);
+
+    return 0;
+}
