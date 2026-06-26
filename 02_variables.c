3ba5759ec4a52840d05d28d0b96bb24381510fba
David Swords <david@swords.computer>
2026-06-26 18:22:57 +0200

    Added 02_variables.c

---
+++ 02_variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int age = 41;
+    double height = 1.78;
+    char initial = 'D';
+
+    printf("Age: %d\n", age);
+    printf("Height: %.2f\n", height);
+    printf("Initial: %c\n", initial);
+
+    return 0;
+}
