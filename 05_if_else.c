2005ff772d2b958ad9102a1d565daf11293c01f2
David Swords <david@swords.computer>
2026-06-26 18:40:40 +0200

    Added 05_if_else.c

---
+++ 05_if_else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int age = 20;
+
+    if (age >= 18) {
+        printf("You are an adult.\n");
+    } else {
+        printf("You are a minor.\n");
+    }
+
+    return 0;
+}
