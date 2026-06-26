1ec9399f75e9d28bbb2e6d4057cfa02486815fc1
David Swords <david@swords.computer>
2026-06-26 18:28:22 +0200

    Added 03_arithmetic.c

---
+++ 03_arithmetic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 10;
+    int b = 3;
+
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    printf("\n");
+
+    printf("Addition:       %d\n", a + b);
+    printf("Subtraction:    %d\n", a - b);
+    printf("Multiplication: %d\n", a * b);
+    printf("Division:       %d\n", a / b);
+    printf("Remainder:      %d\n", a % b);
+
+    return 0;
+}
